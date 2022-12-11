5f8fe6e8e25f7aed6e65f58f66b7a84ad5b4afdd
jaywathakka21600@gmail.com <johnwaithaka@kabarak.ac.ke>
2022-12-11 15:59:46 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:_food_delivery_ui_practice/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async{
+
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: AuthGate(),
   ));
+}
+
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
